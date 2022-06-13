8d241625c8bcdde87b2ec1fa6174742e1445e841
REMAX-SOURCE <107276529+REMAX-SOURCE@users.noreply.github.com>
2022-06-13 17:53:51 +0300

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 by REMAX @REMAX-SOURCE, < https://github.com/REMAX-SOURCE >.
+#
+# This file is part of < https://github.com/REMAX-SOURCE/RemaxmusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/REMAX-SOURCE/RemaxmusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
